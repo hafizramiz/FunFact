ea39d6d51a1e20d958b1bb9d459d7dbcfaeca801
Hafiz <ramizovhafiz@gmail.com>
2023-09-11 16:48:53 +0300

    firs commit

---
+++ app/src/main/java/com/example/funfact/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.funfact
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.funfact.ui.theme.FunFactTheme
+import com.hafizramiz.funfacts.ui.screens.FunFactsNavigationGraph
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FunFactTheme {
+                FunFactApp()
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun FunFactApp() {
+    FunFactsNavigationGraph()
+}
